a5d6700dae33801cc2ed6c962e6a03509308cf1f
Mehrob Latipov <bumerang.passport@gmail.com>
2019-02-07 18:29:19 +0300

    First foreach

--- add.php
+++ add.php
@@ -9,6 +9,9 @@ $object=new stdclass;
 for($i=0;$i<100; $i+=2){
     $arr[]=$i*2;
 }
-echo '<pre>';
-print_r($arr);
-echo '</pre>';
+foreach($arr as $key=>$value){
+    
+    if($key>0  && $key%2 == 0){
+        echo '$key['. $key .'] = value[' .$value .']<br/>';
+    }
+}
