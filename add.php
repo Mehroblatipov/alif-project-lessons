c8a0c8af55b51d5d7a368720fed321c3b1c2fc6b
Mehrob Latipov <bumerang.passport@gmail.com>
2019-02-07 18:52:17 +0300

    First php lesson

--- add.php
+++ add.php
@@ -6,7 +6,7 @@ $f=1.1;
 $arr = [];
 $object=new stdclass;
 
-for($i=0;$i<100; $i+=2){
+for($i=0;$i<10; $i+=2){
     $arr[]=$i*2;
 }
 foreach($arr as $key=>$value){
@@ -17,8 +17,31 @@ foreach($arr as $key=>$value){
 }
 
 
-while($k < 100) 
+while($k < 10) 
 {
   echo "<br/>mehrob";
   $k++;
 }
+$a=103;
+echo "<br/>";
+switch($a){
+    case $a<10;
+        break;
+    case $a < 2;
+        break;
+    case $a < 12;
+        echo '$a =' . $a;
+        break;
+    default:
+        echo "Нет";    
+}
+echo "<br/>";
+$name='Mehrob';
+$name1='Sshabnam';
+echo "$name $name1";
+
+
+$fullname="$name $name1";
+echo $fullname;
+$fullname='$name $name1';
+echo $fullname;
