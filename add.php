f65acf9080db9a93b0facf223135330c3f562646
Mehrob Latipov <bumerang.passport@gmail.com>
2019-02-08 15:11:20 +0300

    TASKS

--- add.php
+++ add.php
@@ -1,49 +1,57 @@
 <?php
-$a=1;
-$b="Mehrob";
-$c="true";
-$f=1.1;
-$arr = [];
-$object=new stdclass;
-
-for($i=0;$i<10; $i+=2){
-    $arr[]=$i*2;
+$s=0;
+for ($i=1; $i<=112; $i+=3){
+   $s=$s+$i;
 }
-foreach($arr as $key=>$value){
-    
-    if($key>0  && $key%2 == 0){
-        echo '$key['. $key .'] = value[' .$value .']<br/>';
+    echo $s;
+    echo "<p></p>"
+?>
+
+<?php
+for ($i=0; $i<100; $i++){
+   $arr[]=$i;
+}
+$a=array_rand($arr);
+$b=array_rand($arr);
+$c=array_rand($arr);
+ if($a!=$b && $a!=$b && $b!=$c )
+    echo "$a $b $c";
+    echo "<p></p>";
+
+
+?>
+
+<?php
+$n=100;
+for ($i=2; $i<=$n; $i++){
+    if ($i%2==0){
+        $arr[$i]=0;
+    }
+    else
+        $arr[$i]=1;
+}
+print_r($arr);
+echo "<p></p>";
+
+?>
+
+<?php
+$array=[1, 0, 6, 9, 4, 5, 2, 3, 8, 7] ; 
+
+for ($j = 0; $j < count($array) - 1; $j++){
+    for ($i = 0; $i < count($array) - $j - 1; $i++){
+        if ($array[$i] > $array[$i + 1]){
+           $tmp = $array[$i + 1];
+            $array[$i + 1] = $array[$i];
+            $array[$i] = $tmp;
+        }
     }
 }
+var_dump($array)
+?>
+<?php
+$array=[1, 0, 6, 9, 4, 5, 2, 3, 8, 7] ; 
+unset($array[3]);
+print_r($array);
 
-
-while($k < 10) 
-{
-  echo "<br/>mehrob";
-  $k++;
-}
-$a=103;
-echo "<br/>";
-switch($a){
-    case $a<10;
-        break;
-    case $a < 2;
-        break;
-    case $a < 12;
-        echo '$a =' . $a;
-        break;
-    default:
-        echo "Нет";    
-}
-echo "<br/>";
-$name='Mehrob';
-$name1='Sshabnam';
-echo "$name $name1";
-
-
-$fullname="$name $name1";
-echo $fullname;
-$fullname='$name $name1';
-echo $fullname;
-
-MEHROBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+?>
